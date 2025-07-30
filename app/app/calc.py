db7a0a6ba0eefb834bb5595badc92093619c2ffd
mohanishParganiha <harshparganiha@gmail.com>
2025-07-30 18:56:10 +0530

    add testing and database

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+calculator function
+"""
+
+def add(x,y): 
+    """adding function to addition of two number returns int result"""
+    return x + y
+
+def sub(x,y):
+    """adding function to subtraction of two number returns int result"""
+    return x-y
---
+++ app/app/test.py
@@ -0,0 +1,25 @@
+"""
+sample test
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTest(SimpleTestCase):
+    # test the calc module
+
+    def test_add_numbers(self):
+        #test adding numbers together
+
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
+
+
+    def test_sub_numbers(self):
+        #test subtracting numbers together
+
+        res = calc.sub(4,2)
+
+        self.assertEqual(res,2)
