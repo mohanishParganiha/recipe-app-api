477f35e9c5e3d17987aed0d1492b0ced32b4c418
mohanishParganiha <harshparganiha@gmail.com>
2025-08-22 09:59:35 +0530

    succesfully created test for admin

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,34 @@
+"""
+test for django admin modifications.
+"""
+
+from django.test import TestCase  # noqa
+from django.contrib.auth import get_user_model  # noqa
+from django.urls import reverse  # noqa
+from django.test import Client  # noqa
+
+
+class AdminSitTests(TestCase):
+    """Test fro Django admin."""
+
+    def setUp(self):
+        """Create user and client,"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User',
+        )
+
+    def test_users_list(self):
+        """test taht users are listed on page."""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
