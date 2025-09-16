fd6e4e2b5b3ac340a73165eac0b68a5631202318
mohanishParganiha <harshparganiha@gmail.com>
2025-09-16 18:54:11 +0530

    created new ingredient test , ingredient model and serializer in nested serializer , and test for creating recipe with new and existing ingredients

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -11,6 +11,7 @@ from rest_framework.permissions import IsAuthenticated  # noqa
 from core.models import (
     Recipe,
     Tag,
+    Ingredient,
 )
 from recipe import serializers
 
@@ -51,4 +52,19 @@ class TagViewSet(mixins.DestroyModelMixin,
 
     def get_queryset(self):
         """filter query set to authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSets(mixins.DestroyModelMixin,
+                         mixins.UpdateModelMixin,
+                         mixins.ListModelMixin,
+                         viewsets.GenericViewSet):
+    """manage ingredietns in the database."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
